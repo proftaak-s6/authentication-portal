7a549772e58dc116faceece480d368eb2d3c039e
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2019-05-24 10:49:11 +0200

    Added dutch role names

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -19,4 +19,15 @@ export class AdminComponent implements OnInit {
   async ngOnInit() {
     this.users = this.userSerivce.getRoleOverview();
   }
+
+  getRoleNames(roles: string[]): string[] {
+    const lookup = {
+      'user': '',
+      'driver': 'Rekeningrijder',
+      'government': 'Overheid',
+      'police': 'Politie'
+    };
+
+    return roles.map(roleName => lookup[roleName]).filter(p => p !== '');
+  }
 }
